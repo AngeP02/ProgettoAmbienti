4d7a37c37ae05386d7ca685a8e4e831ddf8ac03a
AngeP02 <porcoangelica02@outlook.com>
2024-04-20 13:44:22 +0200

    modifiche di contatti

--- app/src/main/java/com/example/progetto/Contatti/ContattiFragment.kt
+++ app/src/main/java/com/example/progetto/Contatti/ContattiFragment.kt
@@ -1,7 +1,10 @@
 package com.example.progetto.Contatti
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,6 +15,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.progetto.R
+import android.provider.MediaStore
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
 
 class ContattiFragment : Fragment() {
 
@@ -33,6 +42,20 @@ class ContattiFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Ottenere i nomi dai metodi del ViewModel
+        val nomeContatto1 = viewModel.getNome1()
+        val cognomeContatto1 = viewModel.getCognome1()
+        val numeroTelefonoContatto1 = viewModel.getNumTelefono1()
+
+        // Trovare i TextView nel layout e impostare i loro valori
+        val textViewNome1 = view.findViewById<TextView>(R.id.Nome1)
+        textViewNome1.text = nomeContatto1
+
+        val textViewCognome1 = view.findViewById<TextView>(R.id.Cognome1)
+        textViewCognome1.text = cognomeContatto1
+
+        val textViewNumTelefono1 = view.findViewById<TextView>(R.id.NumTelefono1)
+        textViewNumTelefono1.text = numeroTelefonoContatto1
 
         val imageViewPhone1 = view.findViewById<ImageView>(R.id.imageView)
         imageViewPhone1.setOnClickListener {
@@ -44,7 +67,76 @@ class ContattiFragment : Fragment() {
             intent.data = Uri.parse("tel:$phoneNumber")
             startActivity(intent)
         }
+        val imageViewMessage1 = view.findViewById<ImageView>(R.id.imageView4)
+        imageViewMessage1.setOnClickListener {
+            // Prendi il numero di telefono dal TextView
+            val phoneNumber = view.findViewById<TextView>(R.id.NumTelefono1).text.toString()
 
+            // Avvia un'intent implicito per aprire l'app dei messaggi
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("smsto:")  // Specifica l'azione per avviare l'app dei messaggi
+
+            // Aggiungi il numero di telefono come dati nell'intent
+            intent.putExtra("address", phoneNumber)
+
+            // Avvia l'intent
+            startActivity(intent)
+        }
+        val imageViewCam1 = view.findViewById<ImageView>(R.id.imageView2)
+        imageViewCam1.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+        // DA FARE L'IMMAGINE DI MODIFICA CON IL POPUP DIALOG
+        // E PER GLI ALTRI FARE IL CONTROLLO SE SONO NULL
+
+    }
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            sendMMS(imageBitmap)
+        }
+    }
+
+    private fun sendMMS(bitmap: Bitmap) {
+        val phoneNumber = view?.findViewById<TextView>(R.id.NumTelefono1)?.text.toString()
+        val uri = saveImageToExternalStorage(bitmap)
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra("sms_body", "Messaggio con immagine")
+        intent.putExtra("address", phoneNumber)
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+        intent.type = "image/*"
+        startActivity(intent)
+    }
+
+    private fun saveImageToExternalStorage(bitmap: Bitmap): Uri? {
+        val wrapper = ContextWrapper(requireContext().applicationContext)
+        var file = wrapper.getDir("images", Context.MODE_PRIVATE)
+        file = File(file, "unique_filename.jpg")
+
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+        return Uri.parse(file.absolutePath)
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/progetto/Contatti/ContattiViewModel.kt
+++ app/src/main/java/com/example/progetto/Contatti/ContattiViewModel.kt
@@ -8,55 +8,345 @@ class ContattiViewModel(private val applicationContext: Context) : ViewModel(){
     private lateinit var dbHelper: MyDatabaseHelper
 
     // Metodi per ottenere i dati dalla base di dati
+
     fun getNome1(): String {
         dbHelper = MyDatabaseHelper(applicationContext)
         val db1 = dbHelper.readableDatabase
 
         val projection = arrayOf(
-            MyDatabaseHelper.COLUMN_EMAILL,
-            MyDatabaseHelper.COLUMN_PASSWORDL
+            MyDatabaseHelper.COLUMN_NOMECONTATTO1
         )
-    return ""
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var nomeContatto1 = ""
+        if (cursor.moveToFirst()) {
+            nomeContatto1 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_NOMECONTATTO1))
+        }
+
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+
+        return nomeContatto1
     }
 
     fun getCognome1(): String {
-        // Sostituisci questa logica con il codice per ottenere il cognome del contatto 1 dalla base di dati
-        return "Cognome Contatto 1"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_COGNOMECONTATTO1
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var cognomeContatto1 = ""
+        if (cursor.moveToFirst()) {
+            cognomeContatto1 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_COGNOMECONTATTO1))
+        }
+
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+
+        return cognomeContatto1
     }
 
     fun getNumTelefono1(): String {
-        // Sostituisci questa logica con il codice per ottenere il numero di telefono del contatto 1 dalla base di dati
-        return "Numero di telefono Contatto 1"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_TELEFONOCONTATTO1
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var telefonoContatto1 = ""
+        if (cursor.moveToFirst()) {
+            telefonoContatto1 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_TELEFONOCONTATTO1))
+        }
+
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+
+        return telefonoContatto1
     }
 
     fun getNome2(): String {
-        // Sostituisci questa logica con il codice per ottenere il nome del contatto 2 dalla base di dati
-        return "Nome Contatto 2"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_NOMECONTATTO2
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var nomeContatto2 = ""
+        if (cursor.moveToFirst()) {
+            nomeContatto2 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_NOMECONTATTO2))
+        }
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+        if (nomeContatto2.isEmpty()) {
+            // Se nomeContatto2 è vuoto, restituisci un valore predefinito o gestisci il caso a tuo piacimento
+            return ""
+        } else {
+            // Altrimenti, restituisci il valore estratto dal database
+            return nomeContatto2
+        }
+
     }
 
     fun getCognome2(): String {
-        // Sostituisci questa logica con il codice per ottenere il cognome del contatto 2 dalla base di dati
-        return "Cognome Contatto 2"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_COGNOMECONTATTO2
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var cognomeContatto2 = ""
+        if (cursor.moveToFirst()) {
+            cognomeContatto2 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_COGNOMECONTATTO2))
+        }
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+        if (cognomeContatto2.isEmpty()) {
+            // Se nomeContatto2 è vuoto, restituisci un valore predefinito o gestisci il caso a tuo piacimento
+            return ""
+        } else {
+            // Altrimenti, restituisci il valore estratto dal database
+            return cognomeContatto2
+        }
     }
 
     fun getNumTelefono2(): String {
-        // Sostituisci questa logica con il codice per ottenere il numero di telefono del contatto 2 dalla base di dati
-        return "Numero di telefono Contatto 2"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_TELEFONOCONTATTO2
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var telefonoContatto2 = ""
+        if (cursor.moveToFirst()) {
+            telefonoContatto2 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_TELEFONOCONTATTO2))
+        }
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+        if (telefonoContatto2.isEmpty()) {
+            // Se nomeContatto2 è vuoto, restituisci un valore predefinito o gestisci il caso a tuo piacimento
+            return ""
+        } else {
+            // Altrimenti, restituisci il valore estratto dal database
+            return telefonoContatto2
+        }
     }
 
     fun getNome3(): String {
-        // Sostituisci questa logica con il codice per ottenere il nome del contatto 3 dalla base di dati
-        return "Nome Contatto 3"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_NOMECONTATTO3
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var nomeContatto3 = ""
+        if (cursor.moveToFirst()) {
+            nomeContatto3 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_NOMECONTATTO3))
+        }
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+        if (nomeContatto3.isEmpty()) {
+            // Se nomeContatto2 è vuoto, restituisci un valore predefinito o gestisci il caso a tuo piacimento
+            return ""
+        } else {
+            // Altrimenti, restituisci il valore estratto dal database
+            return nomeContatto3
+        }
     }
 
     fun getCognome3(): String {
-        // Sostituisci questa logica con il codice per ottenere il cognome del contatto 3 dalla base di dati
-        return "Cognome Contatto 3"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_COGNOMECONTATTO3
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var cognomeContatto3 = ""
+        if (cursor.moveToFirst()) {
+            cognomeContatto3 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_COGNOMECONTATTO3))
+        }
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+        if (cognomeContatto3.isEmpty()) {
+            // Se nomeContatto2 è vuoto, restituisci un valore predefinito o gestisci il caso a tuo piacimento
+            return ""
+        } else {
+            // Altrimenti, restituisci il valore estratto dal database
+            return cognomeContatto3
+        }
     }
 
     fun getNumTelefono3(): String {
-        // Sostituisci questa logica con il codice per ottenere il numero di telefono del contatto 3 dalla base di dati
-        return "Numero di telefono Contatto 3"
+        dbHelper = MyDatabaseHelper(applicationContext)
+        val db1 = dbHelper.readableDatabase
+
+        val projection = arrayOf(
+            MyDatabaseHelper.COLUMN_TELEFONOCONTATTO3
+        )
+        // Definisci i criteri per la query
+        val selection = null // Non ci sono criteri in questo esempio
+        val selectionArgs = null // Non ci sono argomenti per la selezione
+        val sortOrder = null // Non c'è ordinamento specificato
+
+        // Esegui la query
+        val cursor = db1.query(
+            MyDatabaseHelper.TABLE_CONTATTI,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        // Estrai il valore dalla prima riga del cursore
+        var telefonoContatto3 = ""
+        if (cursor.moveToFirst()) {
+            telefonoContatto3 = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_TELEFONOCONTATTO3))
+        }
+        // Chiudi il cursore dopo aver finito
+        cursor.close()
+        if (telefonoContatto3.isEmpty()) {
+            // Se nomeContatto2 è vuoto, restituisci un valore predefinito o gestisci il caso a tuo piacimento
+            return ""
+        } else {
+            // Altrimenti, restituisci il valore estratto dal database
+            return telefonoContatto3
+        }
     }
 
 }
